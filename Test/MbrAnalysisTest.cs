1e18413af0c4c5c337ea574b5b002097ea1bbb90
Alexander <AlexSolivais@gmail.com>
2022-03-10 16:31:19 -0600

    Started MbrAnalysisTest

---
+++ Test/MbrAnalysisTest.cs
@@ -0,0 +1,30 @@
+﻿using Nett;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TaskLayer;
+using EngineLayer;
+
+namespace Test
+{
+    [TestFixture]
+    public static class MbrAnalysisTest
+    {
+        [Test]
+        public static void MbrPostSearchAnalysisTest()
+        {
+            
+            SearchTask searchTask = new SearchTask();
+            
+            //Get SpectralLibrary
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestData\myPrositLib.msp");
+            var testLibraryWithoutDecoy = new SpectralLibrary(new List<string> { path });
+            var librarySpectra = testLibraryWithoutDecoy.GetAllLibrarySpectra().ToList();
+
+
+        }
+
+    }
+}
